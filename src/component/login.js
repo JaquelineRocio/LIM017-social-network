f4a886c2a6ecb00a3dc74006f642e0a9e963568b
ximena silva <ximenasilvacotrina11@gmail.com>
2022-03-28 19:34:31 -0500

    alerta de correo valido

--- src/component/login.js
+++ src/component/login.js
@@ -1,32 +1,38 @@
 /* eslint-disable import/no-cycle */
 /* eslint-disable import/named */
 import { onNavigate } from '../main.js';
-import { signIn, SignInWithGoogle } from '../controllers/auth.js';
-import { mainPage } from './mainPage.js';
+import { signIn, signInWithGoogle, signInWithFacebook } from '../controllers/auth.js';
+// import { mainPage } from './mainPage.js';
 
 export const login = () => {
   const sectionLogin = document.createElement('section');
   sectionLogin.setAttribute('id', 'sectionLogin');
   sectionLogin.innerHTML = `<section id="formLogin">
-  <form>
+  
   <h1>Ingresa</h1>
     <p>
-   <input type = "email" id="mail" class='classInput' placeholder ="Email"/><abbr title="required" aria-label="required">*</abbr> 
+   <input type = "email" id="mail" class='classInput' placeholder ="Email"/><abbr title="required" aria-label="required">*</abbr>
+   <p id="wrongEmail"></p>
    </p>
     <p>
    <input type = "password" id="password" pattern=".{6,}" class='classInput' placeholder ="Contraseña"/><abbr title="required" aria-label="required">*</abbr>
+   <p id="wrongPassword"></p>
    </p>
    <button id="btnLogin" class ="button">Ingresa</button>
    <button type="button" id="signInGoogle" class ="buttonFirebase">Continua con Google</button>
-   </form>
+   <button type="button" id="signInFacebook" class ="buttonFirebase">Continua con Facebook</button>
    </section>
   `;
 
   sectionLogin.querySelector('#btnLogin').addEventListener('click', () => {
-    signIn(sectionLogin.querySelector('#mail').value, sectionLogin.querySelector('#password').value);
-    if (true) { onNavigate('/mainPage'); } else { console.log('no se puede entrar'); }
+    const email = sectionLogin.querySelector('#mail').value;
+    const password = sectionLogin.querySelector('#password').value;
+    const wrongEmail = sectionLogin.querySelector('#wrongEmail');
+    const wrongPassword = sectionLogin.querySelector('#wrongPassword');
+    signIn(email, password, wrongEmail, wrongPassword);
   });
-  sectionLogin.querySelector('#signInGoogle').addEventListener('click', () => SignInWithGoogle());
+  sectionLogin.querySelector('#signInGoogle').addEventListener('click', () => signInWithGoogle());
+  sectionLogin.querySelector('#signInFacebook').addEventListener('click', () => signInWithFacebook());
   // const btnLogin = document.getElementById('btnLogin');
   // btnLogin.addEventListener('click', () => onNavigate('/'));
   return sectionLogin;
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/no-cycle */
 /* eslint-disable max-len */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 // eslint-disable-next-line import/no-unresolved
 import {
-  getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup,
+  getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, FacebookAuthProvider,
 } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
+import { onNavigate } from '../main.js';
 
 export const createUser = (email, password) => {
   const auth = getAuth();
@@ -22,42 +25,75 @@ export const createUser = (email, password) => {
     });
 };
 
-export const signIn = (email, password) => {
+export const signIn = (email, password, wrongEmail, wrongPassword) => {
   const auth2 = getAuth();
   signInWithEmailAndPassword(auth2, email, password)
     .then((userCredential) => {
     // Signed in
       const user2 = userCredential.user;
-      console.log(user2);
+      onNavigate('/mainPage');
     // ...
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      console.log(errorCode, errorMessage);
+      switch (errorCode) {
+        case 'auth/user-not-found':
+          wrongEmail.innerText = 'No existe usuario registrado con ese correo';
+          break;
+        case 'auth/wrong-password':
+          wrongPassword.innerText = 'La contraseña no es válida, intente de nuevo.';
+          break;
+        case 'auth/email-already-exists':
+          wrongEmail.innerText = 'Este correo ya esta registrado, intente de nuevo.';
+          break;
+        default:
+          wrongPassword.innerText = errorCode;
+      }
     });
 };
 
-export const SignInWithGoogle = () => {
+export const signInWithGoogle = () => {
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
   signInWithPopup(auth, provider)
     .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
-      // The signed-in user info.
       const user = result.user;
     // ...
     }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log(errorCode, errorMessage, email, credential);
+    });
+};
+
+export const signInWithFacebook = () => {
+  const provider = new FacebookAuthProvider();
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+    // The signed-in user info.
+      const user = result.user;
+
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+
+    // ...
+    })
+    .catch((error) => {
     // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
       // The email of the user's account used.
       const email = error.email;
       // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-      console.log(errorCode, errorMessage, email, credential);
+      const credential = FacebookAuthProvider.credentialFromError(error);
+
+    // ...
     });
 };
