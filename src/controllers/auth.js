b9204efa0bccbaece082d7b7e11f6044bc7654bc
ximena silva <ximenasilvacotrina11@gmail.com>
2022-03-31 18:59:18 -0500

    sign in with facebook

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -14,7 +14,6 @@ export const createUser = (email, password, wrongEmail, wrongPassword) => {
   const auth = getAuth();
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-    // Signed in
       const user = userCredential.user;
       onNavigate('/mainPage');
     })
@@ -46,7 +45,6 @@ export const signIn = (email, password, wrongEmail, wrongPassword) => {
   let result;
   signInWithEmailAndPassword(auth2, email, password)
     .then((userCredential) => {
-    // Signed in
       const user2 = userCredential.user;
       onNavigate('/mainPage');
     })
@@ -59,6 +57,12 @@ export const signIn = (email, password, wrongEmail, wrongPassword) => {
           break;
         case 'auth/wrong-password':
           wrongPassword.innerText = 'La contraseña no es válida, intente de nuevo.';
+          break;
+        case 'auth/internal-error':
+          wrongEmail.innerText = 'Digite correctamente su contraseña';
+          break;
+        case 'auth/invalid-email':
+          wrongEmail.innerText = 'Digite correctamente su correo y contraseña';
           break;
         case 'auth/email-already-exists':
           wrongEmail.innerText = 'Este correo ya esta registrado, intente de nuevo.';
@@ -93,24 +97,15 @@ export const signInWithFacebook = () => {
   const auth = getAuth();
   signInWithPopup(auth, provider)
     .then((result) => {
-    // The signed-in user info.
       const user = result.user;
-
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      onNavigate('/mainPage');
       const credential = FacebookAuthProvider.credentialFromResult(result);
       const accessToken = credential.accessToken;
-
-    // ...
     })
     .catch((error) => {
-    // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
       const email = error.email;
-      // The AuthCredential type that was used.
       const credential = FacebookAuthProvider.credentialFromError(error);
-
-    // ...
     });
 };
